bb06a8c4e47b856ab20377a7b161bc07ff9f21c9
zaryab2000 <zaryabafser2000@gmail.com>
2020-09-16 18:23:07 +0530

    checking Interest Function

--- contracts/SwamCrowdSale.sol
+++ contracts/SwamCrowdSale.sol
@@ -2,10 +2,12 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
 
-
 contract SwamCrowdsale is Crowdsale{
+    using SafeMath for uint256;
+    
     address public tokenAddress;
     IERC20 public SwamTtoken;
+    uint256 public exchangeRate = 1000; // 1 dollar = 1000 tokens
     
     constructor (
         uint256 rate,
@@ -27,11 +29,23 @@ contract SwamCrowdsale is Crowdsale{
         return SwamTtoken.balanceOf(_user);
     }
     
-    function stake(uint256 _tokens) public{
+    function stake(uint256 _amountInDollars, uint256 _durationMonths) public{
         address user = msg.sender;
-        SwamTtoken.transferFrom(user,address(this),_tokens);
-        userToTokens[user] = _tokens;
+        uint256 tokens = _amountInDollars.mul(exchangeRate);
+        SwamTtoken.transferFrom(user,address(this),tokens);
+        // if(_amountInDollars == 2000 && _durationMonths==1){
+        //     assignInterest(16,user);
+        // }else if(_amountInDollars == 2000 && _durationMonths==3){
+        //     assignInterest(20,user);
+        // }else{
+        //     handleLowInterests(user,_amountInDollars,_durationMonths);
+        // }
+        userToTokens[user] = tokens;
     }
+    
+    // function assignInterest(uint256 _interest, address _user) public{
+        
+    // }
     
 
 }
