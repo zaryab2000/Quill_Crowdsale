a3a1868dea01c20f0cd6525822e749a8954dd817
zaryab2000 <zaryabafser2000@gmail.com>
2020-09-17 20:00:28 +0530

    added staking Function && lockUP func

--- contracts/SwamCrowdSale.sol
+++ contracts/SwamCrowdSale.sol
@@ -2,11 +2,12 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
 
-contract SwamCrowdsale is Crowdsale{
+contract QuillCrowdsale is Crowdsale{
     using SafeMath for uint256;
     
+    uint256 public totalUIDs;
     address public tokenAddress;
-    IERC20 public SwamTtoken;
+    IERC20 public QuillTtoken;
     uint256 public exchangeRate = 1000; // 1 dollar = 1000 tokens
     
     constructor (
@@ -18,42 +19,118 @@ contract SwamCrowdsale is Crowdsale{
         public
         Crowdsale(rate, wallet, ERCtoken)
     {
-        SwamTtoken = ERCtoken;
+        QuillTtoken = ERCtoken;
         tokenAddress = address(ERCtoken);
     
     }
     
+    struct userDetails{
+        uint256 totalStaked;
+        uint256 totalThreeMonthUIDs;
+        uint256 totalOneMonthUIDs;
+        address userAddress;
+        bool staked;       
+    }
+
+    struct lockupDetails{
+        uint256 apy;
+        uint256 tokensLocked;
+        uint256 lockUpTime;
+        uint256 deadLineInDays;
+        uint256 interestEarned;
+        address uid;
+        address owner;
+    }
     
-    mapping(address=>uint256) public userToTokens;
-    mapping(address => bool) public staked;
+    mapping(address => userDetails) public userData;
+    mapping(address => lockupDetails) public lockupData;
+    mapping(uint256 => address) public uidList;
+    mapping(address => mapping(uint256 => lockupDetails)) public threeMonthperiod;
+    mapping(address => mapping(uint256 => lockupDetails)) public oneMonthperiod;
+    
     
     function get(address _user) public view returns(uint256){
-        return SwamTtoken.balanceOf(_user);
+        return QuillTtoken.balanceOf(_user);
     }
     
     function stake(uint256 _amountInDollars) public{
         address user = msg.sender;
+        require(!userData[user].staked,"User has already STAKED");
         uint256 tokens = _amountInDollars.mul(exchangeRate);
-        SwamTtoken.transferFrom(user,address(this),tokens);
+        QuillTtoken.transferFrom(user,address(this),tokens);
         if(_amountInDollars >=2000){
-            staked[user]= true;
+            userData[user].staked = true;
+            assignInterest(16,user,tokens,3);
         }
     }
     
     function lockUp(uint256 _tokens,uint256 _numberOfMonths) public{
-        if(staked[msg.sender] && _numberOfMonths==3){
-            assignInterest(20,msg.sender,_tokens);
-        }else if(staked[msg.sender] && _numberOfMonths==1){
-            assignInterest(16,msg.sender,_tokens);
-        }else if(!staked[msg.sender] && _numberOfMonths==1){
-            assignInterest(12,msg.sender,_tokens);
-        }else if(!staked[msg.sender] && _numberOfMonths==3){
-            assignInterest(16,msg.sender,_tokens);
+        if(userData[msg.sender].staked && _numberOfMonths==3){
+            assignInterest(20,msg.sender,_tokens,_numberOfMonths);
+        }else if(userData[msg.sender].staked && _numberOfMonths==1){
+            assignInterest(16,msg.sender,_tokens,_numberOfMonths);
+        }else if(!userData[msg.sender].staked && _numberOfMonths==1){
+            assignInterest(12,msg.sender,_tokens,_numberOfMonths);
+        }else if(!userData[msg.sender].staked && _numberOfMonths==3){
+            assignInterest(16,msg.sender,_tokens,_numberOfMonths);
         }
     }
     
-    function assignInterest(uint256 _interest, address _user,uint256 _tokens) public{
+    function assignInterest(uint256 _interest, address _user,uint256 _tokens,uint256 _numberOfMonths) public{
+        require(QuillTtoken.balanceOf(_user)>_tokens, "User Balance is Not Enough");
+        QuillTtoken.transferFrom(_user,address(this),_tokens);
+        
+        address uid = address(bytes20(keccak256(abi.encodePacked(msg.sender,now))));
+        totalUIDs++;
+        
+        lockupDetails memory details = lockupDetails(_interest,_tokens,now,_numberOfMonths.mul(10),0,uid,_user);
+        lockupData[uid] = details;
+        userDetails memory user = userData[_user];
+        user.totalStaked += _tokens;
+        user.userAddress = _user;
+        if(_numberOfMonths == 3){
+            user.totalThreeMonthUIDs += 1;
+            threeMonthperiod[_user][user.totalThreeMonthUIDs] = details;
+        }else if(_numberOfMonths == 1){
+            user.totalOneMonthUIDs += 1;
+            oneMonthperiod[_user][user.totalOneMonthUIDs] = details;
+        }
+        
+        userData[_user] = user;
+        uidList[totalUIDs] = uid;
         
     }
+    
+    
+    function updateInterest(address _uid) public{
+        require(lockupData[_uid].owner == msg.sender, "Caller is not the OWNER");
+        require(now > lockupData[_uid].lockUpTime + 7 * 1 days, "Interest can be updated only after 7 days");
+        uint256 principalAmt = lockupData[_uid].tokensLocked;
+        uint256 apy = lockupData[_uid].apy;
+        uint256 interest = apy.mul(principalAmt).div(100);
+        uint256 updatedBalance = principalAmt.add(interest);
+        
+        lockupDetails memory _data = lockupData[_uid];
+        _data.interestEarned += interest;
+        _data.tokensLocked += updatedBalance;
+        _data.lockUpTime = now;
+        
+        lockupData[_uid] = _data;
+        
+        
+    }
+    
+    function withdrawLockedTokens(address _uid) public{
+        require(lockupData[_uid].owner == msg.sender, "Caller is not the OWNER");
+        uint256 deadline = lockupData[_uid].deadLineInDays;
+        require(now > lockupData[_uid].lockUpTime + deadline * 1 days, "LockUP period NOT OVER YET");
+
+        uint256 withdrawAmount = lockupData[_uid].tokensLocked;
+        QuillTtoken.transfer(msg.sender,withdrawAmount);
+        
+        
+    }
+    
+    
     
 }
--- contracts/SwamToken.sol
+++ contracts/SwamToken.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
 import "./ERC20.sol";
-contract SwamToken{
+contract QuillToken{
     ERC20 public token;
     address public tokenAddress;
 
