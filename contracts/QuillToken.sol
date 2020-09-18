04c29e752ae61e5a27a6c70a10d1fbf85ef2a103
zaryab2000 <zaryabafser2000@gmail.com>
2020-09-18 20:28:06 +0530

    added owner call function

--- contracts/QuillToken.sol
+++ contracts/QuillToken.sol
@@ -4,13 +4,16 @@ import "./ERC20.sol";
 contract QuillToken{
     ERC20 public token;
     address public tokenAddress;
+    address public owner;
 
     constructor(string memory _name,string memory _symbol,uint256 _totalAmount) public{
         token = new ERC20(_name,_symbol,_totalAmount);
         tokenAddress = address(token);
+        owner = msg.sender;
     }
     
     function transferFunds(address _crowdsale,uint256 _amt) public{
+        require(msg.sender == owner, "Caller is not the Owner");
         token = ERC20(tokenAddress);
         token.transfer(_crowdsale,_amt);
     }   
